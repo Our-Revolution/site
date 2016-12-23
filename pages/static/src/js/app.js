30cc148e64d301964a499abb206852d291d88563
Chris Mabry <cjmab28@gmail.com>
2016-12-23 16:07:08 -0600

    Much better page

--- pages/static/src/js/app.js
+++ pages/static/src/js/app.js
@@ -3,4 +3,5 @@ window.ListFuzzySearch = require('./vendor/list.fuzzysearch.min.js');
 window.Cookies = window.Cookies = require('js-cookie');
 
 var bootstrap = require('bootstrap-sass'),
-  List = require('list.js');
+  List = require('list.js'),
+  validator = require('bootstrap-validator');
