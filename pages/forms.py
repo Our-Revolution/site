1ad1210d0116508ec1207e9341847a2794dd89ac
Chris Mabry <cjmab28@gmail.com>
2017-03-09 18:48:22 -0600

    Merge

--- pages/forms.py
+++ pages/forms.py
@@ -1,11 +1,9 @@
-from django import forms
+import django.forms as forms
 from local_groups.models import Group
 from phonenumber_field.widgets import PhoneNumberPrefixWidget, PhoneNumberInternationalFallbackWidget
 from phonenumber_field.modelfields import PhoneNumberField
 
-
 class GroupForm(forms.ModelForm):
-
     required_css_class = 'required'
     
     class Meta:
@@ -16,7 +14,7 @@ class GroupForm(forms.ModelForm):
             'last_meeting': forms.DateInput(),
             'description': forms.Textarea(attrs={'rows':'5'}),
             'other_social': forms.Textarea(attrs={'rows':'2'}),
-            'other_issues': forms.Textarea(attrs={'rows':'3'}),
+            'other_issues': forms.Textarea(attrs={'rows':'4'}),
             'constituency': forms.Textarea(attrs={'rows':'3'}),
             'issues': forms.CheckboxSelectMultiple
         }
