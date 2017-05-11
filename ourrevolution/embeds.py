46e035c40bc59d5eac09c5a775fd0fdc12caf714
bahoo <jon.c.culver@gmail.com>
2017-05-11 11:15:39 -0700

    There we go

--- ourrevolution/embeds.py
+++ ourrevolution/embeds.py
@@ -63,7 +63,7 @@ def oembed_monkeypatched(url, max_width=None):
     oembed = json.loads(r.read().decode('utf-8'))
 
     # Convert photos into HTML
-    if oembed['type'] in ('photo', 'image'):
+    if oembed['type'] == 'photo':
         html = '<img src="%s" />' % (oembed['url'], )
     else:
         html = oembed.get('html')
