baf72f55ff816c2f7e9041572e1dcbb132c265d3
Chris Mabry <cjmab28@gmail.com>
2017-05-25 20:36:51 -0500

    New form placeholder

--- nominations/urls.py
+++ nominations/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import GroupNominationsIndexView
+from .views import NominationsIndexView
 
 urlpatterns = [
-    url(r'^groups/nominations', GroupNominationsIndexView.as_view())
+    url(r'^groups/nominations', NominationsIndexView.as_view()),
+    url(r'^groups/nominations/new', NewNominationForm.as_view())
+    
 ]
--- nominations/views.py
+++ nominations/views.py
@@ -3,10 +3,33 @@ from django.views.generic.base import TemplateView
 
 # Create your views here.
 
-class GroupNominationsIndexView(TemplateView):
+class NominationsIndexView(TemplateView):
     
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
-        context = super(GroupNominationsIndexView, self).get_context_data(**kwargs)
+        context = super(NominationsIndexView, self).get_context_data(**kwargs)
         return context
+
+class NewNominationForm(forms.Form):
+    email = forms.EmailField(label="Your Email Address", help_text="We'll send your Slack invite here.")
+    full_name = forms.CharField(required=False, label="Your Full Name", help_text="Optional")
+    state = forms.ChoiceField(label="Invite me to a specific Slack channel", help_text="You can join others once you log in.",initial="C36GU58J0")
+
+    def __init__(self, *args, **kwargs):
+        
+        super(SlackInviteForm, self).__init__(*args, **kwargs)
+
+        channel_names = {
+            'gis-nerdery': 'GIS Nerdery',
+            'nc-research': 'NC Research',
+            'techprojects': 'Tech Projects',
+        }
+
+        # fetch Slack channels
+        req = requests.get("https://slack.com/api/channels.list?token=%s" % os.environ['LOCAL_OR_ORGANIZING_API_TOKEN'])
+        channel_choices = [(c['id'], channel_names.get(c['name'], c['name'].replace('_', ' ').title())) for c in req.json()['channels']]
+
+        channel_choices.insert(0, (None, 'None'))
+
+        self.fields['state'].choices = channel_choices
