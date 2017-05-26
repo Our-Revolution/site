f7b72e6dd1d1d7ac05b88c75e1cf27257ec1c2ce
Chris Mabry <cjmab28@gmail.com>
2017-05-26 15:43:05 -0500

    Remove placeholder form, add forms.py

--- nominations/urls.py
+++ nominations/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls import url
 from .views import NominationsIndexView
 
 urlpatterns = [
-    url(r'^groups/nominations', NominationsIndexView.as_view()),
-    url(r'^groups/nominations/new', NewNominationForm.as_view())
-    
+    url(r'^groups/nominations', NominationsIndexView.as_view())
 ]
