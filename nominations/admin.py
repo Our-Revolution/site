29d43baacecf843743c0af6e873cd89a5213fecd
Chris Mabry <cjmab28@gmail.com>
2017-06-05 14:56:34 -0500

    Nominations models

--- nominations/admin.py
+++ nominations/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Nomination
+from .models import *
 
 # Register your models here.
 @admin.register(Nomination)
 class NominationAdmin(admin.ModelAdmin):
     list_display = ['group_name']
+
+@admin.register(Questionnaire)
+class QuestionnaireAdmin(admin.ModelAdmin):
+    list_display = ['candidate_first_name','candidate_last_name','candidate_office','candidate_state']
