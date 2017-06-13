0e612a4ad1a5a0431349d842a8df1f4d1f80799f
Chris Mabry <cjmab28@gmail.com>
2017-06-13 17:57:34 -0500

    application form layout

--- nominations/forms.py
+++ nominations/forms.py
@@ -8,31 +8,50 @@ from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field, F
 from models import Nomination, Application
 
 class NewApplicationForm(forms.ModelForm):
+    group_name = forms.CharField(label="Group Name")
+    group_id = forms.CharField(label='Group ID')
+    
     #crispy forms 
     def __init__(self, *args, **kwargs):
         super(NewApplicationForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.form_method = 'post'
-        self.helper.add_input(Submit('submit', 'Start a Nomination'))
+        self.helper.form_action = ''
+        self.helper.form_class = 'row'
+        
         self.helper.layout = Layout(
-            Fieldset(
-                'Group Representative',
-                'rep_email',
-                'rep_first_name',
-                'rep_last_name',
-                'rep_phone',
-                css_class='mb20 mt20',
+            Div(
+                HTML('<div class="col-md-12"><h2>Your Group Information</h2></div>'),
+                Div(
+                    Field('group_name',wrapper_class='col-md-8'),
+                    Field('group_id',wrapper_class='col-md-4'),
+                    Field('rep_first_name',wrapper_class='col-md-6'),
+                    Field('rep_last_name',wrapper_class='col-md-6'),
+                    Field('rep_phone',wrapper_class='col-md-6'),
+                    Field('rep_email',wrapper_class='col-md-6'),
+                ),
+                css_class="mb20 clearfix",
             ),
-            Fieldset(
-                'Candidate Information',
-                'candidate_first_name',
-                'candidate_last_name',
-                'candidate_office',
-                'candidate_state',
-                css_class='mb20 mt20',
+            Div(
+                HTML('<div class="col-md-12"><h2>Candidate Information</h2></div>'),
+                Div(
+                    Field('candidate_first_name',wrapper_class='col-md-6'),
+                    Field('candidate_last_name',wrapper_class='col-md-6'),
+                    Field('candidate_office',wrapper_class='col-md-6'),
+                    Field('candidate_state',wrapper_class='col-md-6'),
+                ),
+                css_class='mb_20 clearfix'
             ),
+            Div(
+                Submit(
+                    'submit',
+                    'Start a Nomination',
+                    css_class='btn btn-primary btn-block uppercase ls2'
+                ),
+                css_class='col-md-12'
+            )
         )
             
     class Meta:
         model = Application
-        fields = ['rep_email','rep_first_name','rep_last_name','rep_phone','candidate_first_name','candidate_last_name','candidate_office','candidate_state']
+        fields = ['group_name','group_id','rep_email','rep_first_name','rep_last_name','rep_phone','candidate_first_name','candidate_last_name','candidate_office','candidate_state']
