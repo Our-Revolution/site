72ed44c88b4360b85ef976f3a44b28f5cd0994e7
cjmabry <cjmab28@gmail.com>
2017-07-21 16:29:35 -0500

    supprt not lat long on groups

--- pages/static/src/js/groups.js
+++ pages/static/src/js/groups.js
@@ -1,29 +1,29 @@
-module.exports = function() {  
+module.exports = function() {
   var map, active = null, usedSearch = false, data = {}, mapboxToken = 'pk.eyJ1Ijoib3VycmV2b2x1dGlvbiIsImEiOiJjaXpzdm91M3UwMDA3MzNvY2NhZXZtZ21hIn0.WBmvgFv12o8eEnv2GsZthA';
-    
+
   function init(mapDiv) {
     initMap(mapDiv);
   }
-  
-  function initMap(mapDiv) {  
+
+  function initMap(mapDiv) {
     map = L.map(mapDiv).setView([37.8, -96.9], 4);
-    
+
     var baseLayer = L.tileLayer('https://api.mapbox.com/styles/v1/ourrevolution/cj1tl1d07001h2slqtzp1o8s5/tiles/256/{z}/{x}/{y}?access_token=' + mapboxToken, {
-        maxZoom: 18 
+        maxZoom: 18
       }
     ).addTo(map);
-    
+
     $(map).on('zoomend', function() {
-      
+
       // only do this after user has used search box
       if (usedSearch) {
         getLocationsWithinBounds(map.getBounds());
-      }  
+      }
     });
-    
+
     // populate(map);
   }
-  
+
   function getAddress() {
     var geometry, place = autocomplete.getPlace();
     usedSearch = true;
@@ -42,16 +42,16 @@ module.exports = function() {
 
     }
   }
-  
-  function positionMap(geometry) {       
+
+  function positionMap(geometry) {
     map.flyTo(geometry, 10, {duration:1});
   }
-  
+
   function monitorAPI(input) {
     var defaultPlaceholder = input.placeholder;
 
     setTimeout(function(){
-      
+
         input.value = '';
         setInterval(function(){
           if(input.placeholder.indexOf('Oops') != -1){
@@ -69,49 +69,52 @@ module.exports = function() {
         }, 200);
     }, 500);
   }
-  
+
   function addFeature(feature) {
     feature.addTo(map);
   }
-  
+
   function paintGeoJson(geometry, options) {
     var feature = L.geoJson(geometry, options);
-    
+
     feature.addTo(map);
   }
-  
+
   function addGroup(group) {
-    var coords = group.geometry.coordinates;
-    var marker = L.marker(coords.reverse());
-    
-    marker.properties = group.properties;
-    
-    marker.bindPopup('<h4>'+ group.properties.name + '</h1>');
-    
-    marker.on('click touchstart', function() {
-      updateInfo([this]);
-    });
-    
-    marker.addTo(map);
-    
+    if (group.geometry) {
+      var coords = group.geometry.coordinates;
+      var marker = L.marker(coords.reverse());
+
+      marker.properties = group.properties;
+
+      marker.bindPopup('<h4>'+ group.properties.name + '</h1>');
+
+      marker.on('click touchstart', function() {
+        updateInfo([this]);
+      });
+
+      marker.addTo(map);
+    } else {
+      console.log(group.properties.name + ' has no geometry.');
+    }
   }
-  
+
   function resetInfo() {
     $('.groups-map-info__status').html('');
   }
-  
+
   function updateInfo(groups) {
     var location = '', city, county, state, country;
-    
+
     resetInfo();
-                
-    if(groups.length != 0){    
-      for (var i=0; i<groups.length; i++) {    
+
+    if(groups.length != 0){
+      for (var i=0; i<groups.length; i++) {
         city = groups[i].properties.city;
         county = groups[i].properties.county;
         state = groups[i].properties.state;
         country = groups[i].properties.country;
-        
+
         if (country == 'US') {
           if (city && state) {
             location = city + ', ' + state;
@@ -135,10 +138,10 @@ module.exports = function() {
             location = country;
           }
         }
-        
+
         // TODO: show recurring meeting here instead
         description = groups[i].properties.description;
-        
+
         $('.groups-map-info__status').append('\
           <div class="component">\
             <div class="component__heading">\
@@ -151,7 +154,7 @@ module.exports = function() {
           </div>\
         ');
       }
-    } else {      
+    } else {
       $('.groups-map-info__status').append('\
         <div class="groups-map__add relative">\
         <h4 class="mb20 mt0">We don\'t have any groups here yet.</h4>\
@@ -161,47 +164,47 @@ module.exports = function() {
         <p class="mt20"><small>We\'re currently experiencing a high number of group applications,\ which may lead to delays in adding your group to the map.</small></p>\
       </div>');
     }
-    
+
   }
-  
+
   function getLocationsWithinBounds(bounds) {
     var layers = [], t0 = performance.now();
-    
+
     map.eachLayer(function (layer) {
-      
+
       if(layer.properties) {
         if(bounds.contains(layer._latlng)) {
           layers.push(layer);
         }
       }
     });
-    
+
     updateInfo(layers.reverse());
     var t1 = performance.now();
     usedSearch = false;
   }
-  
+
   function setActive(layer) {
     layer.options.oldColor = layer.options.color;
-    
+
     if(active) {
       var oldLayer = map._layers[active];
       oldLayer.setStyle({"color": oldLayer.options.oldColor})
-    } 
-    
+    }
+
     layer.setStyle({"color":"#78a515"});
-    
+
     active = layer._leaflet_id;
   }
-  
+
   function getActive() {
     return active;
   }
-  
+
   function getMap() {
     return map;
   }
-  
+
   return {
     init: init,
     paintGeoJson: paintGeoJson,
@@ -212,8 +215,8 @@ module.exports = function() {
     updateInfo: updateInfo,
     getLocationsWithinBounds: getLocationsWithinBounds,
     setActive: setActive,
-    getActive: getActive, 
+    getActive: getActive,
     monitorAPI: monitorAPI,
     addGroup: addGroup
-  };   
+  };
 }();
