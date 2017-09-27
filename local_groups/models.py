b237e672887ac6733535bd8bbd1870b30a84de7f
Eric Broder <eric@ourrevolution.com>
2017-09-27 14:40:59 -0700

    add notes field to groups table

--- local_groups/models.py
+++ local_groups/models.py
@@ -10,37 +10,37 @@ from recurrence.fields import RecurrenceField
 from django.contrib.gis.db.models import PointField
 
 
-class Group(models.Model):    
+class Group(models.Model):
     name = models.CharField(max_length=64, null=True, blank=False, verbose_name="Group Name")
     slug = models.SlugField(null=True, blank=False, unique=True, max_length=100)
     signup_date = models.DateTimeField(null=True, blank=True, auto_now_add=True)
     group_id = models.CharField(max_length=4,null=True, blank=False, unique=True)
-    
+
     rep_email = models.EmailField(null=True, blank=False, verbose_name="Contact Email", max_length=254)
     rep_first_name = models.CharField(max_length=35, null=True, blank=False, verbose_name="First Name")
     rep_last_name = models.CharField(max_length=35, null=True, blank=False, verbose_name="Last Name")
     rep_postal_code = models.CharField(max_length=12, null=True, blank=True, verbose_name="Postal Code")
     rep_phone = PhoneNumberField(null=True, blank=True, verbose_name="Phone Number")
-    
+
     county = models.CharField(max_length=64, null=True, blank=True)
     city = models.CharField(max_length=64, null=True, blank=True)
     state = USStateField(max_length=2, null=True, blank=True)
     postal_code = models.CharField(max_length=12, null=True, blank=True, verbose_name="Postal Code")
     country = CountryField(null=True, blank=False, default="US")
     point = PointField(null=True, blank=True)
-        
+
     size = models.CharField(max_length=21, null=True, blank=True, verbose_name="Group Size")
-    
+
     last_meeting = models.DateTimeField(null=True, blank=True, verbose_name="Date of Last Meeting")
     recurring_meeting = RecurrenceField(null=True, blank=True, verbose_name="Recurring Meeting")
-    
+
     meeting_address_line1 = models.CharField("Address Line 1", max_length = 45, null=True, blank=True)
     meeting_address_line2 = models.CharField("Address Line 2", max_length = 45, null=True, blank=True)
     meeting_postal_code = models.CharField("Postal Code", max_length = 12, null=True, blank=True)
     meeting_city = models.CharField(max_length = 64, null=True, blank=True, verbose_name="City")
     meeting_state_province = models.CharField("State/Province", max_length = 40, null=True, blank=True)
     meeting_country = CountryField(null=True, blank=True, verbose_name="Country", default='US')
-    
+
     TYPES_OF_ORGANIZING_CHOICES = (
         ('direct-action', 'Direct Action'),
         ('electoral', 'Electoral Organizing'),
@@ -50,13 +50,13 @@ class Group(models.Model):
     )
     types_of_organizing = MultiSelectField(null=True, blank=True, choices=TYPES_OF_ORGANIZING_CHOICES, verbose_name="Types of Organizing")
     other_types_of_organizing = models.TextField(null=True, blank=True, verbose_name="Other Types of Organizing", max_length=500)
-    
+
     description = models.TextField(null=True, blank=False, max_length=1000, verbose_name="Description (1000 characters or less)")
     issues = models.ManyToManyField(Issue, blank=True)
     other_issues = models.TextField(null=True, blank=True, max_length=250, verbose_name="Other Issues")
-    
+
     constituency = models.TextField(null=True, blank=True, max_length=250)
-    
+
     facebook_url = models.URLField(null=True, blank=True, verbose_name="Facebook URL", max_length=255)
     twitter_url = models.URLField(null=True, blank=True, verbose_name="Twitter URL", max_length=255)
     website_url = models.URLField(null=True, blank=True, verbose_name="Website URL", max_length=255)
@@ -64,39 +64,49 @@ class Group(models.Model):
     other_social = models.TextField(null=True, blank=True, verbose_name="Other Social Media", max_length=250)
 
     state_organizing_committee = models.BooleanField(default=False)
-    
+
     STATUSES = (
        ('submitted', 'Submitted'),
        ('signed-mou', 'Signed MOU'),
        ('inactive', 'Inactive'),
        ('approved', 'Approved'),
        ('removed', 'Removed') # can flesh out later
-    ) 
+    )
     status = models.CharField(max_length=64, choices=STATUSES, default='submitted')
-    
+
     VERSIONS = (
        ('none', 'N/A'),
        ('1.0', 'Old'),
        ('1.1', 'Current'),
-   ) 
-    
+   )
+
     signed_mou_version = models.CharField(max_length=64, choices=VERSIONS, default='none', verbose_name='MOU Version', null=True, blank=True)
-    
+
     ORGANIZERS = (
        ('juliana', 'Juliana'),
        ('basi', 'Basi'),
        ('kyle', 'Kyle'),
     )
-    
+
     organizer = models.CharField(max_length=64, choices=ORGANIZERS, default=None, verbose_name='Organizer', null=True, blank=True)
-    
+
     mou_url = models.URLField(null=True, blank=True, verbose_name="MOU URL", max_length=255)
-    
+
+    # Notes field for internal OR staff use
+    notes = models.TextField(
+        blank=True,
+        help_text=  """Please include dates here along with notes to make
+                    reporting easier.""",
+        null=True,
+        verbose_name="Notes"
+    )
+
+
     # source = models.CharField(max_length=17, null=True, blank=True)
     # subsource = models.FloatField(null=True, blank=True)
     # i_p__address = models.CharField(max_length=16, null=True, blank=True)
     # constituent__id = models.IntegerField(null=True, blank=True)
     # signup__id = models.IntegerField(null=True, blank=True)
-        
+
     def __unicode__(self):
         return self.name
