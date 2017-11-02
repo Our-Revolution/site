70545b2a56789539a7f800d7d5de6ce6face0ec2
Eric Broder <eric@ourrevolution.com>
2017-11-01 18:02:45 -0700

    TECH-355 add notification banner to cms

--- pages/templatetags/pages_tags.py
+++ pages/templatetags/pages_tags.py
@@ -3,10 +3,10 @@ from pages.models import *
 
 register = template.Library()
 
-# Notification Banner snippets that are set to show
+# Notification Banner snippet set to show
 @register.inclusion_tag('pages/tags/notification-banner.html', takes_context=True)
 def notification_banner(context):
     return {
-        'notifications': NotificationBanner.objects.filter(show=True),
+        'notification_banner': NotificationBanner.objects.filter(show=True).first(),
         'request': context['request'],
     }
