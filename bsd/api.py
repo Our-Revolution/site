1ceb605908f0655912cab33d7130272a112094cf
Eric Broder <eric@ourrevolution.com>
2017-11-15 18:47:16 -0800

    TECH-290 add BSD authentication client

--- bsd/api.py
+++ bsd/api.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
 from bsdapi.BsdApi import Factory as BsdApiFactory
+from django.conf import settings
 
-BSD_API_HOST = os.environ['BSD_API_HOST']
-BSD_API_ID = os.environ['BSD_API_ID']
-BSD_API_SECRET = os.environ['BSD_API_SECRET']
 
 class Bsd:
     api = BsdApiFactory().create(
-        id = BSD_API_ID,
-        secret = BSD_API_SECRET,
-        host = BSD_API_HOST,
+        id = settings.BSD_API_ID,
+        secret = settings.BSD_API_SECRET,
+        host = settings.BSD_API_HOST,
         port = 80,
         securePort = 443
     )
