0145e1ced49ff872b296cd1ac42042ef67162e02
Eric Broder <eric@ourrevolution.com>
2018-01-25 13:47:32 -0800

    TECH-650 update results for 2018

--- pages/templatetags/pages_tags.py
+++ pages/templatetags/pages_tags.py
@@ -1,8 +1,14 @@
 from django import template
 from django.conf import settings
-from pages.models import *
+from pages.models import NotificationBanner
 
 register = template.Library()
+
+
+@register.simple_tag
+def candidates_url():
+    return settings.CANDIDATES_URL
+
 
 # Navigation menu
 @register.inclusion_tag('partials/nav.html', takes_context=True)
@@ -12,10 +18,31 @@ def navigation_menu(context):
         'request': context['request'],
     }
 
+
 # Notification Banner snippet set to show
-@register.inclusion_tag('pages/tags/notification_banner.html', takes_context=True)
+@register.inclusion_tag(
+    'pages/tags/notification_banner.html',
+    takes_context=True
+)
 def notification_banner(context):
     return {
-        'notification_banner': NotificationBanner.objects.filter(show=True).first(),
+        'notification_banner': NotificationBanner.objects.filter(
+            show=True
+        ).first(),
         'request': context['request'],
     }
+
+
+@register.simple_tag
+def results_url():
+    return settings.RESULTS_URL
+
+
+@register.simple_tag
+def results_2016_url():
+    return settings.RESULTS_2016_URL
+
+
+@register.simple_tag
+def results_2017_url():
+    return settings.RESULTS_2017_URL
