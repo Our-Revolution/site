633b7a32da84f709e614a4ce1b5fb274b337a37b
Eric Broder <eric@ourrevolution.com>
2018-03-07 22:03:53 -0800

    TECH-722 add password reset pages to organizing hub

--- local_groups/urls.py
+++ local_groups/urls.py
@@ -61,11 +61,6 @@ if settings.BSD_LOGIN_ENABLED:
                         name='password_reset',
                     ),
                     url(
-                        r'^complete/',
-                        auth_views.password_reset_complete,
-                        name='password_reset_complete',
-                    ),
-                    url(
                         r'^confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
                         GroupPasswordResetView.as_view(),
                         name='password_reset_confirm',
