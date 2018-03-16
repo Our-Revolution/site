53c9a6d4c9c1216f8a05927db09cdf9049720b84
Eric Broder <eric@ourrevolution.com>
2018-03-16 12:27:00 -0700

    TECH-889 add transform party index page

--- transform/models.py
+++ transform/models.py
@@ -33,5 +33,37 @@ class TransformPartyPage(Page):
 
 
 class TransformPartyIndexPage(Page):
+    # body = RichTextField()
+    # signup_embed_code = models.TextField(
+    #     blank=True,
+    #     null=True,
+    #     help_text='Raw HTML embed code for signup form, etc.'
+    # )
+    # social_image = models.ForeignKey(
+    #     'wagtailimages.Image',
+    #     null=True,
+    #     blank=True,
+    #     on_delete=models.SET_NULL,
+    #     related_name='+'
+    # )
+    #
+    # content_panels = Page.content_panels + [
+    #     FieldPanel('signup_embed_code'),
+    #     FieldPanel('body'),
+    # ]
+    # promote_panels = Page.promote_panels + [
+    #     ImageChooserPanel('social_image')
+    # ]
+
     parent_page_types = ['pages.IndexPage']
     subpage_types = ['transform.TransformPartyPage']
+
+    def get_context(self, *args, **kwargs):
+        context = super(TransformPartyIndexPage, self).get_context(
+            *args,
+            **kwargs
+        )
+        context['transform_party_pages'] = self.get_children().live().order_by(
+            'title',
+        )
+        return context
