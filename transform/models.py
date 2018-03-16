cd3fb19ec1d73b92a5f6072d07c910bcdcf5dc1d
Eric Broder <eric@ourrevolution.com>
2018-03-16 14:20:04 -0700

    TECH-889 add transform party index page

--- transform/models.py
+++ transform/models.py
@@ -33,27 +33,42 @@ class TransformPartyPage(Page):
 
 
 class TransformPartyIndexPage(Page):
-    # body = RichTextField()
-    # signup_embed_code = models.TextField(
-    #     blank=True,
-    #     null=True,
-    #     help_text='Raw HTML embed code for signup form, etc.'
-    # )
-    # social_image = models.ForeignKey(
-    #     'wagtailimages.Image',
-    #     null=True,
-    #     blank=True,
-    #     on_delete=models.SET_NULL,
-    #     related_name='+'
-    # )
-    #
-    # content_panels = Page.content_panels + [
-    #     FieldPanel('signup_embed_code'),
-    #     FieldPanel('body'),
-    # ]
-    # promote_panels = Page.promote_panels + [
-    #     ImageChooserPanel('social_image')
-    # ]
+    primary_content = RichTextField(
+        default='',
+    )
+    primary_content_background_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    secondary_content = RichTextField(
+        blank=True,
+        null=True,
+    )
+    signup_embed_code = models.TextField(
+        blank=True,
+        null=True,
+        help_text='Raw HTML embed code for signup form, etc.'
+    )
+    social_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('primary_content'),
+        ImageChooserPanel('primary_content_background_image'),
+        FieldPanel('secondary_content'),
+        FieldPanel('signup_embed_code'),
+    ]
+    promote_panels = Page.promote_panels + [
+        ImageChooserPanel('social_image')
+    ]
 
     parent_page_types = ['pages.IndexPage']
     subpage_types = ['transform.TransformPartyPage']
