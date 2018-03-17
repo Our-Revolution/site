d284a80dee3a8e14e5b80cf74451bca7a14d9bc6
Eric Broder <eric@ourrevolution.com>
2018-03-16 19:01:10 -0700

    TECH-889 add transform party index page

--- transform/models.py
+++ transform/models.py
@@ -1,9 +1,16 @@
 from __future__ import unicode_literals
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+from wagtail.contrib.wagtailfrontendcache.utils import purge_page_from_cache
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.signals import page_published, page_unpublished
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class TransformPartyPage(Page):
@@ -30,6 +37,39 @@ class TransformPartyPage(Page):
         ImageChooserPanel('social_image')
     ]
     subpage_types = []
+
+
+'''
+Purge transform party index page when transform party page changes
+
+http://docs.wagtail.io/en/v1.10.1/reference/contrib/frontendcache.html
+'''
+
+
+def transform_party_page_changed(transform_party_page):
+    logger.debug('transform_party_page_changed')
+    """Purge parent page"""
+    parent_page = transform_party_page.get_parent()
+    logger.debug('parent: ' + str(parent_page))
+    purge_page_from_cache(parent_page)
+
+
+@receiver(pre_delete, sender=TransformPartyPage)
+def transform_party_deleted_handler(instance, **kwargs):
+    logger.debug('transform_party_deleted_handler')
+    transform_party_page_changed(instance)
+
+
+@receiver(page_published, sender=TransformPartyPage)
+def transform_party_published_handler(instance, **kwargs):
+    logger.debug('transform_party_published_handler')
+    transform_party_page_changed(instance)
+
+
+@receiver(page_unpublished, sender=TransformPartyPage)
+def transform_party_unpublished_handler(instance, **kwargs):
+    logger.debug('transform_party_unpublished_handler')
+    transform_party_page_changed(instance)
 
 
 class TransformPartyIndexPage(Page):
