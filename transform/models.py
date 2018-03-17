83f09647ed5bcee263c0bd83080f3539d8e6d4be
Eric Broder <eric@ourrevolution.com>
2018-03-16 19:02:08 -0700

    TECH-889 add transform party index page

--- transform/models.py
+++ transform/models.py
@@ -47,28 +47,23 @@ http://docs.wagtail.io/en/v1.10.1/reference/contrib/frontendcache.html
 
 
 def transform_party_page_changed(transform_party_page):
-    logger.debug('transform_party_page_changed')
     """Purge parent page"""
     parent_page = transform_party_page.get_parent()
-    logger.debug('parent: ' + str(parent_page))
     purge_page_from_cache(parent_page)
 
 
 @receiver(pre_delete, sender=TransformPartyPage)
 def transform_party_deleted_handler(instance, **kwargs):
-    logger.debug('transform_party_deleted_handler')
     transform_party_page_changed(instance)
 
 
 @receiver(page_published, sender=TransformPartyPage)
 def transform_party_published_handler(instance, **kwargs):
-    logger.debug('transform_party_published_handler')
     transform_party_page_changed(instance)
 
 
 @receiver(page_unpublished, sender=TransformPartyPage)
 def transform_party_unpublished_handler(instance, **kwargs):
-    logger.debug('transform_party_unpublished_handler')
     transform_party_page_changed(instance)
 
 
