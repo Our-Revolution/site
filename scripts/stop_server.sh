6bfee65fc633ed1f9d173103874902aea59322e2
Eric Broder <eric@ourrevolution.com>
2018-05-08 16:17:55 -0700

    TECH-1045 test prod deploy

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -5,7 +5,18 @@ cd /home/ubuntu/ourrevolution
 source /home/ubuntu/.virtualenvs/ourrevolution/bin/virtualenvwrapper.sh
 workon ourrevolution
 
+if [ "$DEPLOYMENT_GROUP_NAME" == "QA" ]
+then
+    echo qa deployment group: $DEPLOYMENT_GROUP_NAME
+fi
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "Production" ]
+then
+    echo Production deployment group: $DEPLOYMENT_GROUP_NAME
+fi
+
 echo "Stop server"
+supervisord -c supervisord.conf
 supervisorctl stop gunicorn
 
 echo "Finished script for stopping server"
