a8020132a2c4b15c6051c351fa68a3a5ffc52b65
Eric Broder <eric@ourrevolution.com>
2018-05-08 21:58:33 -0700

    TECH-1045 test prod deploy

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -5,22 +5,15 @@ cd /home/ubuntu/ourrevolution
 source /home/ubuntu/.virtualenvs/ourrevolution/bin/virtualenvwrapper.sh
 workon ourrevolution
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "QA" ]
-then
-    echo "qa deployment group: $DEPLOYMENT_GROUP_NAME"
-else
-    echo "not qa deploy group"
-fi
-
 if [ "$DEPLOYMENT_GROUP_NAME" == "Production" ]
 then
-    echo "Production deployment group: $DEPLOYMENT_GROUP_NAME"
+    echo "Stop varnish"
+    sudo service varnish stop
 else
-    echo "not Production deploy group"
+    echo "Skip varnish"
 fi
 
 echo "Stop server"
-supervisord -c supervisord.conf
 supervisorctl stop gunicorn
 
 echo "Finished script for stopping server"
