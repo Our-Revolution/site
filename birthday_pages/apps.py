44db728bdf557c81de08494ff0330f6ce8e9cd26
Eric Broder <eric@ourrevolution.com>
2018-08-09 19:11:22 -0700

    TECH-1335 create birthday page

--- birthday_pages/apps.py
+++ birthday_pages/apps.py
@@ -2,5 +2,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class TransformConfig(AppConfig):
-    name = 'transform'
+class BirthdayPagesConfig(AppConfig):
+    name = 'birthday_pages'
