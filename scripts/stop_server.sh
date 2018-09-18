518f8f68e6a75bb1981962ad31bd88f721ff2b10
Eric Broder <eric@ourrevolution.com>
2018-09-17 21:40:50 -0700

    TECH-1311 deploy celery changes gracefully

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -10,8 +10,8 @@ source /home/ubuntu/.virtualenvs/ourrevolution/bin/virtualenvwrapper.sh
 # TODO: TECH-1294 debug error code
 workon ourrevolution || true
 
-echo "Stop Celery main process and let tasks finish"
-# Stop main process only with warm shutdown so worker tasks can finish
+echo "Stop Celery"
+# Stop main process only with graceful shutdown so worker tasks can finish
 celery -A ourrevolution inspect stats | grep '"pid": ' | awk '{print $2}' | awk -F, '{print $1}' | xargs kill -TERM
 
 echo "Stop server"
