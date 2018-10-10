cf6e2075a8df4dbb4998fdbe0a44ea514408124a
Eric Broder <eric@ourrevolution.com>
2018-10-10 14:40:45 -0700

    TECH-1457 fix celery memory leak

--- celery.sh
+++ celery.sh
@@ -2,4 +2,4 @@
 cd /home/ubuntu/ourrevolution
 source /home/ubuntu/.virtualenvs/ourrevolution/bin/virtualenvwrapper.sh
 workon ourrevolution
-exec celery worker -A ourrevolution --loglevel=INFO --concurrency=1 --broker=$CELERY_BROKER_URL --result-backend=$CELERY_RESULT_BACKEND
+exec celery worker -A ourrevolution --loglevel=INFO --concurrency=1 --max-tasks-per-child=1 --broker=$CELERY_BROKER_URL --result-backend=$CELERY_RESULT_BACKEND
