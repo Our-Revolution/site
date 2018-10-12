721e51a0bb19b2233a5f8203e54a8e81f7c7ae9e
Eric Broder <eric@ourrevolution.com>
2018-10-12 14:35:01 -0700

    TECH-1237 add call page

--- organizing_hub/views/call_views.py
+++ organizing_hub/views/call_views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, FormView, TemplateView
 from django.views.generic.list import ListView
@@ -42,7 +43,7 @@ class CallView(
     FormView
 ):
     form_class = CallForm
-    # template_name = 'contact.html'
+    template_name = 'calls/call_form.html'
     # local_group = None
     # permission_required = 'calls.add_callcampaign'
     # success_message = '''
@@ -56,6 +57,8 @@ class CallView(
         # form.send_email()
         return super(CallView, self).form_valid(form)
 
+    def get(self, request, *args, **kwargs):
+        return redirect('organizing-hub-call-dashboard')
     # def form_valid(self, form):
     #     """Set local group and owner before save"""
     #     form.instance.local_group = self.get_local_group()
@@ -143,52 +146,36 @@ class CallDashboardView(LoginRequiredMixin, TemplateView):
         user = self.request.user
         if hasattr(user, 'callprofile'):
             call_profile = user.callprofile
-            campaigns_as_admin = find_campaigns_as_admin(call_profile)
-            campaigns_as_admin_sorted = sorted(
-                campaigns_as_admin,
-                key=lambda x: x.is_in_progress,
-                reverse=True,
-            )
-            campaigns_as_admin_active = [
-                (x, CallForm(initial={
-                    'campaign_uuid': x.uuid
-                })) for x in campaigns_as_admin_sorted if x.is_active
-            ]
-            campaigns_as_admin_inactive = [
-                (x, CallForm(initial={
-                    'campaign_uuid': x.uuid
-                })) for x in campaigns_as_admin_sorted if not x.is_active
-            ]
-            campaigns_as_caller = find_campaigns_as_caller(call_profile)
-            campaigns_as_caller_sorted = sorted(
-                campaigns_as_caller,
-                key=lambda x: x.is_in_progress,
-                reverse=True,
-            )
-            campaigns_as_caller_active = [
-                (x, CallForm(initial={
-                    'campaign_uuid': x.uuid
-                })) for x in campaigns_as_caller_sorted if x.is_active
-            ]
-
-            # """Get Make Calls form for all in progress campaigns"""
-            # call_forms = {}
-            # for campaign in (
-            #     campaigns_as_admin_active + campaigns_as_caller_active
-            # ):
-            #     if campaign.is_in_progress:
-            #         call_form = CallForm(initial={
-            #             'campaign_uuid': campaign.uuid
-            #         })
-            #         call_forms[str(campaign.uuid)] = call_form
-
-            context['campaigns_as_admin_active'] = campaigns_as_admin_active
-            context['campaigns_as_admin_inactive'] = campaigns_as_admin_inactive
-            context['campaigns_as_caller_active'] = campaigns_as_caller_active
-            # context['call_forms'] = call_forms
-            logger.debug(context['campaigns_as_admin_active'])
         else:
             """Create call profile if doesn't exist"""
-            CallProfile.objects.create(user=user)
+            call_profile = CallProfile.objects.create(user=user)
+
+        campaigns_as_admin = find_campaigns_as_admin(call_profile)
+        campaigns_as_admin_sorted = sorted(
+            campaigns_as_admin,
+            key=lambda x: x.is_in_progress,
+            reverse=True,
+        )
+        campaigns_as_admin_active = [(
+            x,
+            CallForm() if x.is_in_progress else None
+        ) for x in campaigns_as_admin_sorted if x.is_active]
+        campaigns_as_admin_inactive = [
+            (x, None) for x in campaigns_as_admin_sorted if not x.is_active
+        ]
+        campaigns_as_caller = find_campaigns_as_caller(call_profile)
+        campaigns_as_caller_sorted = sorted(
+            campaigns_as_caller,
+            key=lambda x: x.is_in_progress,
+            reverse=True,
+        )
+        campaigns_as_caller_active = [(
+            x,
+            CallForm() if x.is_in_progress else None
+        ) for x in campaigns_as_caller_sorted if x.is_active]
+
+        context['campaigns_as_admin_active'] = campaigns_as_admin_active
+        context['campaigns_as_admin_inactive'] = campaigns_as_admin_inactive
+        context['campaigns_as_caller_active'] = campaigns_as_caller_active
 
         return context
