9caf4997b927e62e198ea6d8b115676db1e04b1d
Eric Broder <eric@ourrevolution.com>
2018-10-19 17:07:55 -0700

    TECH-1286 add opt out call response logic

--- calls/apps.py
+++ calls/apps.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
 class CallsConfig(AppConfig):
     name = 'calls'
+
+    def ready(self):
+        import signals
