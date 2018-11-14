5eae7d1b505575e8b8b33bee289e6c81aa1d8f5f
Chris Mabry <cjmab28@gmail.com>
2018-11-14 17:31:40 -0600

    TECH-627/TECH-1548 add method to clear all cache from fastly on demand

--- pages/frontendcache/utils.py
+++ pages/frontendcache/utils.py
@@ -1,10 +1,6 @@
 import logging
 
-import requests
-from django.conf import settings
 from wagtail.contrib.wagtailfrontendcache.utils import get_backends
-
-import fastly
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +12,7 @@ def purge_all_from_cache(backend_settings=None, backends=None):
     for backend_name, backend in get_backends(backend_settings=backend_settings, backends=backends).items():
         """Check that backend has purge_all implemented"""
         try:
+            logger.info("[%s] Purging All", backend_name)            
             backend.purge_all()
-            logger.info("[%s] Purging All", backend_name)
         except AttributeError:
             logger.error("[%s] Backend does not support purge_all.", backend_name)
