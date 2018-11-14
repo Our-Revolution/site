d79b6acf1da4c32431f00f73c7982c18f001c0dc
Eric Broder <eric@ourrevolution.com>
2018-11-13 21:53:09 -0800

    TECH-1398 purge fastly by surrogate key

--- pages/frontendcache/backends.py
+++ pages/frontendcache/backends.py
@@ -13,8 +13,8 @@ logger = logging.getLogger(__name__)
 FASTLY_API_KEY = settings.FASTLY_API_KEY
 FASTLY_SERVICE_ID = settings.FASTLY_SERVICE_ID
 
-api = fastly.API()
-api.authenticate_by_key(FASTLY_API_KEY)
+fastly_api = fastly.API()
+fastly_api.authenticate_by_key(FASTLY_API_KEY)
 
 
 class FastlyBackend(HTTPBackend):
@@ -23,17 +23,22 @@ class FastlyBackend(HTTPBackend):
         self.api_key = params.pop('API_KEY')
         self.hosts = params.pop('HOSTS')
 
+    def get_surrogate_key_for_url(self, url):
+
+        """Get surrogate key from url. Get path without trailing slash."""
+        surrogate_key = urlparse.urlparse(url).path
+
+        """Strip trailing slash unless homepage"""
+        if surrogate_key != "/":
+            surrogate_key = surrogate_key.rstrip('/')
+
+        return surrogate_key
+
     def purge(self, url):
-        """Get Surrogate Key"""
-        skey = urlparse.urlparse(url).path.rstrip('/')
-        logger.debug('skey: %s' % skey)
 
-        for host in self.hosts:
-            api.purge_key(FASTLY_SERVICE_ID, skey)
+        """Get surrogate key from url. Get path without trailing slash."""
+        surrogate_key = self.get_surrogate_key_for_url(url)
+        logger.debug('surrogate_key: %s' % surrogate_key)
 
-            # req = requests.request(
-            #     'PURGE',
-            #     urlparse.urljoin(host, urlparse.urlparse(url).path),
-            #     headers={'Fastly-Key': self.api_key}
-            # )
-            # assert req.status_code == 200
+        """Purge surrogate key"""
+        fastly_api.purge_key(FASTLY_SERVICE_ID, surrogate_key)
