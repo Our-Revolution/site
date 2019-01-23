cb3e9c0b16f1c9d345eb70201304a2375aa29c30
Eric Broder <eric@ourrevolution.com>
2019-01-23 10:17:56 -0800

    TECH-1605 add all info to view questionnaire page

--- nominations/urls.py
+++ nominations/urls.py
@@ -21,9 +21,30 @@ from .views import (
 from django.views.generic import TemplateView
 from .decorators import is_authenticated_candidate
 
-# placeholder urls to code templates
 urlpatterns = [
     url(r'^groups/nominations/', include([
+
+        # Local Group facing pages
+        url(
+            r'^$',
+            NominationsIndexView.as_view(),
+            name='nominations-index'
+        ),
+        url(
+            r'^application/$',
+            ApplicationView.as_view(),
+            name='nominations-application'
+        ),
+        url(
+            r'^application-type/$',
+            ApplicationTypeView.as_view(),
+            name='nominations-application-type',
+        ),
+        url(
+            r'^dashboard/$',
+            DashboardView.as_view(),
+            name='nominations-dashboard'
+        ),
         url(
             r'^email-success/$',
             login_required(TemplateView.as_view(
@@ -31,66 +52,69 @@ urlpatterns = [
             )),
             name='nominations-questionnaire-sent',
         ),
-        url(
-            r'^dashboard/$',
-            DashboardView.as_view(),
-            name='nominations-dashboard'
-        ),
+        url(r'^initiatives/', include([
+            url(
+                r'^new/$',
+                CreateInitiativeView.as_view(),
+                name='nominations-initiative-create'
+            ),
+            url(r'^success/$', login_required(TemplateView.as_view(
+                template_name='success.html'
+            ))),
+        ])),
         url(r'^logout/$', logout),
-        url(
-            r'^application/$',
-            ApplicationView.as_view(),
-            name='nominations-application'
-        ),
-        url(
-            r'^questionnaire/$',
-            QuestionnaireIndexView.as_view(),
-            name='nominations-questionnaire'
-        ),
         url(
             r'^new/$',
             CreateApplicationView.as_view(),
             name='nominations-application-create',
         ),
-        url(
-            r'^application-type/$',
-            ApplicationTypeView.as_view(),
-            name='nominations-application-type',
-        ),
         url(r'^nomination/$', EditNominationView.as_view()),
-        url(
-            r'^questionnaire/edit$',
-            EditQuestionnaireView.as_view(),
-            name='nominations-questionnaire-edit'
-        ),
-        url(
-            r'^questionnaire/reset$',
-            reset_questionnaire,
-            name='nominations-questionnaire-reset'
-        ),
-        url(
-            r'^$',
-            NominationsIndexView.as_view(),
-            name='nominations-index'
-        ),
-    ])),
-    url(r'^groups/nominations/candidate/', include([
-        url(r'^login/$', candidate_login),
-        url(r'^verify/$', TemplateView.as_view(template_name='candidate/verify.html')),
-        url(r'^callback/$', handle_candidate_callback),
-        url(r'^questionnaire/$', is_authenticated_candidate(CandidateQuestionnaireView.as_view())),
-        url(r'^dashboard/$', is_authenticated_candidate(CandidateDashboardView.as_view())),
-        url(r'^submit/$', is_authenticated_candidate(CandidateSubmitView.as_view())),
-        url(r'^success/$', is_authenticated_candidate(TemplateView.as_view(template_name='candidate/success.html'))),
-    ])),
-    url(r'^groups/nominations/initiatives/', include([
-        url(
-            r'^new/$',
-            CreateInitiativeView.as_view(),
-            name='nominations-initiative-create'
-        ),
-        url(r'^success/$', login_required(TemplateView.as_view(
-            template_name='success.html'
-        ))),
+        url(r'^questionnaire/', include([
+            url(
+                r'^$',
+                QuestionnaireIndexView.as_view(),
+                name='nominations-questionnaire'
+            ),
+            url(
+                r'^edit$',
+                EditQuestionnaireView.as_view(),
+                name='nominations-questionnaire-edit'
+            ),
+            url(
+                r'^reset$',
+                reset_questionnaire,
+                name='nominations-questionnaire-reset'
+            ),
+        ])),
+
+        # Candidate facing pages
+        url(r'^candidate/', include([
+            url(r'^callback/$', handle_candidate_callback),
+            url(
+                r'^dashboard/$',
+                is_authenticated_candidate(CandidateDashboardView.as_view())
+            ),
+            url(r'^login/$', candidate_login),
+            url(
+                r'^questionnaire/$',
+                is_authenticated_candidate(
+                    CandidateQuestionnaireView.as_view()
+                )
+            ),
+            url(
+                r'^submit/$',
+                is_authenticated_candidate(CandidateSubmitView.as_view())
+            ),
+            url(
+                r'^success/$',
+                is_authenticated_candidate(TemplateView.as_view(
+                    template_name='candidate/success.html'
+                ))
+            ),
+            url(
+                r'^verify/$',
+                TemplateView.as_view(template_name='candidate/verify.html')
+            ),
+        ])),
     ])),
 ]
