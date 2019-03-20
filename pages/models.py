a044f5bdd3cf959c02dbd2bd66586441f963d378
Eric Broder <eric@ourrevolution.com>
2019-03-20 15:31:31 -0700

    DEV-1753 add featured groups

--- pages/models.py
+++ pages/models.py
@@ -1870,32 +1870,29 @@ class GroupPage(RoutablePageMixin, Page):
         groups_data = json.dumps(data)
 
         """Get featured groups and sort by city, state"""
-        # groups_sorted = sorted(
-        #     groups,
-        #     key=lambda x: (
-        #         (x.state if x.state is not None else 'ZZZ'),
-        #         # (x.city if x.city is not None else 'ZZZ'),
-        #         # x.name,
-        #     ),
-        # )
+        groups_sorted = sorted(
+            groups,
+            key=lambda x: (
+                # (x.state if x.state is not None else 'ZZZ'),
+                (x.city if x.city is not None else 'ZZZ'),
+                x.name,
+            ),
+        )
         # featured_groups = []
         # for group in groups_sorted:
         #     if group.group_rating is not None and group.group_rating >= 3:
         #         featured_groups.append(group)
         featured_groups_by_state = defaultdict(list)
-        for group in groups:
+        for group in groups_sorted:
             if group.group_rating is not None and group.group_rating >= 3:
-                featured_groups_by_state[group.state].append(
+                featured_groups_by_state[group.get_state_display()].append(
                     group
                 )
         # featured_groups = featured_groups_by_state.items
         featured_groups = sorted(
             featured_groups_by_state.iteritems(),
             key=lambda (k, v): (
-                k if k is not None else 'ZZZ'
-                # (x.state if x.state is not None else 'ZZZ'),
-                # (x.city if x.city is not None else 'ZZZ'),
-                # x.name,
+                (k if k is not None else 'ZZZ'),
             ),
         )
 
